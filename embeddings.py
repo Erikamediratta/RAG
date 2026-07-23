ec4e3f2ed6ba6fc2bc3d1123d9c51a56839b9aad
Erika Mediratta <erikamediratta@gmail.com>
2026-07-23 15:20:20 +0530

    Fixing memory issues during deployment

--- embeddings.py
+++ embeddings.py
@@ -1,7 +1,16 @@
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+#Lazy loading the model so that web server is started succesfully before model is loaded
+#solves memory issues on render deployment
+model=None
+def get_model():
+    global model
+    if model is None:
+        model=SentenceTransformer("all-MiniLM-L6-v2")
+    return model
+
 
 def generate_embedding(text):
+    model=get_model()
     return model.encode(text).tolist()
 

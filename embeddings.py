46aa3ebcb72e95e8a1708eca3b589980573125e7
Erika Mediratta <erikamediratta@gmail.com>
2026-07-23 15:58:37 +0530

    Switch to fastembed to fix Render memory crash

--- embeddings.py
+++ embeddings.py
@@ -1,17 +1,10 @@
+from fastembed import TextEmbedding
 
-#Lazy loading the model so that web server is started succesfully before model is loaded
-#solves memory issues on render deployment
-model=None
-def get_model():
-    global model
-    if model is None:
-        from sentence_transformers import SentenceTransformer
-
-        model=SentenceTransformer("all-MiniLM-L6-v2")
-    return model
-
+model = TextEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
 def generate_embedding(text):
-    model=get_model()
-    return model.encode(text).tolist()
+    embedding = list(model.embed([text]))[0]
+    return embedding.tolist()
+
+
 
